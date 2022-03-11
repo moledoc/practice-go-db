5618d12fcea420e8c015169c28b6f870db032b1a
meelis.utt <meelis.utt@gmail.com>
2022-03-11 15:15:01 +0200

    bugfix cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 set -e
+rm -f rest/rest.go
 if [ $(sudo docker container ls | grep pg | wc -l) -gt 0 ];
 then
+	printf "Closing pg container\n"
 	sudo docker stop pg > /dev/null
 	sudo docker rm pg > /dev/null
+fi
+if [ $(sudo docker container ls | grep rest | wc -l) -gt 0 ];
+then
+	printf "Closing rest container\n"
+	sudo docker stop rest > /dev/null
 fi
 # sudo docker rmi postgres:latest
 printf "Removing docker volumes\n"
@@ -13,5 +20,4 @@ printf "Removing docker images\n"
 sudo docker image ls | grep none | awk '{print $3}' | tail -n +1 | xargs -I {} sudo docker image rm "{}"
 printf "Removing docker network prac\n"
 sudo docker network rm prac > /dev/null
-rm -f rest/rest.go
 printf "DONE\n"
