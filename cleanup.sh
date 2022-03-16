6f712d4f60781ea8bdad45cb0deedd948977ca17
meelis.utt <meelis.utt@gmail.com>
2022-03-16 12:19:11 +0200

    Add additional check for cleanup script for removing containers

--- cleanup.sh
+++ cleanup.sh
@@ -2,13 +2,13 @@
 
 set -e
 rm -f rest/rest.go
-if [ $(sudo docker container ls | grep pg | wc -l) -gt 0 ];
+if [ $(sudo docker container ls | grep pg | wc -l) -gt 0 ] || [ $(sudo docker ps -a | grep pg | wc -l) -gt 0 ];
 then
 	printf "Closing pg container\n"
 	sudo docker stop pg > /dev/null
 	sudo docker rm pg > /dev/null
 fi
-if [ $(sudo docker container ls | grep rest | wc -l) -gt 0 ];
+if [ $(sudo docker container ls | grep rest | wc -l) -gt 0 ] || [ $(sudo docker ps -a | grep rest | wc -l) -gt 0 ];
 then
 	printf "Closing rest container\n"
 	sudo docker stop rest > /dev/null
