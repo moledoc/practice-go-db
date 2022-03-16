cec18dc306f5cee146d3d2dc79824e3650511b32
meelis.utt <meelis.utt@gmail.com>
2022-03-16 12:19:35 +0200

    add comments how to clear port 5432, if it's already in use

--- init.sh
+++ init.sh
@@ -7,6 +7,10 @@ then
 	sudo docker network create -d bridge prac
 fi
 printf "Creating and running docker container\n"
+# When port 5432 is already in use, run the following command
+# sudo ss -lptn 'sport = :5432'
+# find pid and then run
+# kill <pid>
 sudo docker run --name pg -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=db -p 5432:5432 --network prac -v postgre:/data -d postgres:latest
 sleep 3
 printf "Init database ddl\n"
@@ -15,3 +19,4 @@ go run init.go
 cd ..
 sudo docker inspect -f '{{.NetworkSettings.Networks.prac.IPAddress}}' pg > .dbip
 go run gen_rest.go
+
